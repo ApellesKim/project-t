894e9922e1e32dd52fed025f68df6224f344e6e1
ApellesKim <90055859+ApellesKim@users.noreply.github.com>
2025-03-03 13:30:17 +0000

    Adding play again & exit game option

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -84,14 +84,35 @@ class TicTacToeGame:
         """Return a toggled player."""
         self.current_player=next(self._players)
 
+    def reset_game(self):
+        """Reset the game state to play again."""
+        for row, row_content in enumerate(self._current_moves):
+            for col, _ in enumerate(row_content):
+                row_content[col]=Move(row, col)
+        self._has_winner=False
+        self.winner_combo=[]
+
 class TicTacToeBoard(tk.Tk):
     def __init__(self, game):
         super().__init__()
         self.title("Tic-Tac-Toe Game")
         self._cells ={}
         self._game=game
+        self._create_menu()
         self._create_board_display()
         self._create_board_grid()
+
+    def _create_menu(self):
+        menu_bar=tk.Menu(master=self)
+        self.config(menu=menu_bar)
+        file_menu=tk.Menu(master=menu_bar)
+        file_menu.add_command(
+            label="Play Again!",
+            command=self.reset_board
+        )
+        file_menu.add_separator()
+        file_menu.add_command(label="Exit", command=self.reset_board)
+        menu_bar.add_cascade(label="File", menu=file_menu)
 
     def _create_board_display(self):
         display_frame = tk.Frame(master=self)
@@ -159,6 +180,15 @@ class TicTacToeBoard(tk.Tk):
         for button, coordinates in self._cells.items():
             if coordinates in self._game.winner_combo:
                 button.config(highlightbackground="red")
+
+    def reset_board(self):
+        self._game.reset_game()
+        self._update_display(msg="Ready?")
+        for button in self._cells.keys:
+            button.config(highlightbackground="lightblue")
+            button.config(text="")
+            button.config(fg="black")
+
         
                 
     
